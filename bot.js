63ecb703c71fbf451a7d38c460acd013e52124a2
Muhammed Ali Aktaş <mhmmdlkts@gmail.com>
2020-04-26 17:06:14 +0200

    more voices and mic off alert

--- bot.js
+++ bot.js
@@ -8,22 +8,14 @@ var connection;
 
 client.on('ready', () => {
   console.log(`${client.user.tag} geldi!`);
+  console.log();
 });
 
 client.on('message', msg => {
   if (msg.content === 'ayberk') {
-    msg.reply('oç erol!');
+    msg.reply('Selam');
   }
   if (msg.content === 'a') {
-    if(client.guildMember !== undefined)
-      msg.reply('not undefined');
-      else {
-
-          msg.reply('undefined');
-      }
-      connection.play('./ho_ayberk.mp3');
-    //const dispatcher = connection.playFile('./ho_ayberk.mp3');
-    //PlayQuote('703338019842949130',"./ho_ayberk.mp3");
   }
 });
 
@@ -31,40 +23,46 @@ client.on("voiceStateUpdate", (oldMember, newMember) => {
 
   if(connection == undefined || client.voice.connections.get("551446105116901403") == undefined)
     return;
-
   var newUserChannel = newMember.channelID;
   var oldUserChannel = oldMember.channelID;
   var clientChannel = client.voice.connections.get("551446105116901403").channel.id;
 
   if(oldUserChannel !== clientChannel && newUserChannel === clientChannel) {
     if(newMember.id == 321792266425335808) // Kutay
-      connection.play('./voice/ho_amdin.mp3');
+      connection.play(getRandomHoVoice("kutay",1));
     if(newMember.id == 384411841637646346) // Ayberk
-      connection.play('./voice/ho_ayberk.mp3');
+      connection.play(getRandomHoVoice("ayberk",2));
     if(newMember.id == 303483041047773197) // Mali
-      connection.play('./voice/ho_amdin.mp3');
+      connection.play(getRandomHoVoice("mali",2));
     if(newMember.id == 323849177610321922) // Faruk
-      connection.play('./voice/ho_faruk.mp3');
+      connection.play(getRandomHoVoice("faruk",1));
     if(newMember.id == 469229854647124008) // Mars
-      connection.play('./voice/ho_mars.mp3');
+      connection.play(getRandomHoVoice("mars",1));
     if(newMember.id == 197620487197556736) // Aliman
-      connection.play('./voice/ho_aliman.mp3');
+      connection.play(getRandomHoVoice("aliman",1));
     if(newMember.id == 319085533026320385) // Erol
-      connection.play('./voice/ho_erol.mp3');
+      connection.play(getRandomHoVoice("erol",1));
     if(newMember.id == 276501498282770453) // Ali Eren
-      connection.play('./voice/ho_alieren.mp3');
+      connection.play(getRandomHoVoice("alieren",1));
     if(newMember.id == 269504150268674048) // Ilker
-      connection.play('./voice/ho_ilker.mp3');
+      connection.play(getRandomHoVoice("ilker",1));
     if(newMember.id == 701199061109309542) // Misa
-      connection.play('./voice/ho_misaa.mp3');
+      connection.play(getRandomHoVoice("misa",1));
     if(newMember.id == 325613866807590913) // Ozi
-      connection.play('./voice/ho_ozi.mp3');
+      connection.play(getRandomHoVoice("ozi",2));
     if(newMember.id == 334706769765072896) // Yagiz
-      connection.play('./voice/ho_yagiz.mp3');
+      connection.play(getRandomHoVoice("yagiz",1));
+    if(newMember.id == 305288732360179722) // Amac
+      connection.play(getRandomHoVoice("amac",1));
 
   } else if(oldUserChannel === clientChannel && newUserChannel !== clientChannel){
 
     // User leaves a voice channel
+
+  } else if(newMember.selfMute && !oldMember.selfMute){
+
+    connection.play("./voice/mic_off.mp3");
+    // User unmute his self
 
   }
 
@@ -104,4 +102,12 @@ function PlayQuote(voiceChannel, file) {
 
 function test() {
   return i + 1;
+}
+
+function getRandomHoVoice(name, count) {
+  return "./voice/ho_" + name + randGen(count) + ".mp3";
+}
+
+function randGen(max) {
+  return Math.floor((Math.random() * max) + 1);
 }
